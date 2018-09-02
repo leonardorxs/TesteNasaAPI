7c40a5cc7212921d9bb4bb241f53028b447460a1
Leonardo Rodrigues <leonardo.rxs@hotmail.com>
2018-09-02 16:22:43 -0300

    API application

---
+++ TesteNasaAPI/Models/NASAImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TesteNasaAPI.Models
+{
+    public class NASAImage
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string MediaType { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- TesteNasaAPI/Pages/Index.cshtml.cs
+++ TesteNasaAPI/Pages/Index.cshtml.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using TesteNasaAPI.Models;
 
 namespace TesteNasaAPI.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public string ImageDate { get; set; }
 
+        public void OnGet([FromServices] IConfiguration config)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var baseUrl = config.GetSection("NASA_OpenAPIs:BaseURL").Value;
+                var key = config.GetSection("NASA_OpenAPIs:Key").Value;
+                var baseDate = DateTime.Now.Date.AddDays(new Random().Next(0, 7) * -1);
+                var response = client.GetAsync(
+                    $"{baseUrl}apod?api_key={key}&date={baseDate:yyyy-MM-dd}").Result;
+
+                response.EnsureSuccessStatusCode();
+                var content = response.Content.ReadAsStringAsync().Result;
+
+                dynamic result = JsonConvert.DeserializeObject(content);
+
+                var image = new NASAImage()
+                {
+                    Title = result.title,
+                    Description = result.explanation,
+                    Url = result.url,
+                    MediaType = result.media_type,
+                    Date = baseDate
+                };
+
+                TempData["NASAImage"] = image;
+
+            }
         }
     }
 }
